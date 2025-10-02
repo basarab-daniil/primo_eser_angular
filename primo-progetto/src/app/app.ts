03741d348dbd5a8da42cf57167b851d3bdd7a450
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2025-10-02 07:36:00 +0000

    final commit

--- primo-progetto/src/app/app.ts
+++ primo-progetto/src/app/app.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HelloWorldComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('primo-progetto');
+  protected readonly title = 'primo-progetto';
 }
+
